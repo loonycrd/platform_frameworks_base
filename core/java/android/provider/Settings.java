3b2e63ab50583283bb7af639b0593ec3362c69fd
Max <xyyx@mail.ru>
2019-07-03 11:57:30 +0000

    Unlock keystore with fingerprint after reboot (1/2)
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5306,6 +5306,12 @@ public final class Settings {
         public static final String FONT_PACKAGES = "font_packages";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
